f4101528f63de2a51b56f95fc2e4e7836b7b6bf0
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-10-22 14:07:47 +0200

    src added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import Menu from '../Menu';
+import Categories from '../Categories';
+import items from '../data';
 
 //Dynamic categories:
 const allCategories=['all', ...new Set(items.map(item=>item.category))];
